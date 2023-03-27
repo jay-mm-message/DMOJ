59ac934adef7d563b96ab70f1f1153166906fdda
jay <jay.mm.message@gmail.com>
2023-03-27 21:44:08 +0800

    This is answer of ccc18j1

---
+++ ccc18j1.py
@@ -0,0 +1,9 @@
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+number_4 = int(input())
+
+if (number_1 == 9 or number_1 == 8) and (number_4 == 9 or number_4 == 8) and (number_2 == number_3):
+  print('ignore')
+else:
+  print('answer')
