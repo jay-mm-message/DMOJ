4e10e869ca0380a74b6fd0576e1616c8d34131d5
jay <jay.mm.message@gmail.com>
2023-04-01 01:23:39 +0800

    Refactor this is answer of coci08c1p2

--- coci08c1p2.py
+++ coci08c1p2.py
@@ -3,56 +3,24 @@ exam_num = int(input())
 exam_consists = list(input())
 exam_current_max = 0
 
-# Adrian
+ADRIAN  = 'ABC'
+BRUNO   = 'BABC'
+GORAN   = 'CCAABB'
+
 Adrian_exam_current_num = 0
-for i in range(exam_num):
-  if i % 3 == 0:
-    if 'A' == exam_consists[i]:
-      Adrian_exam_current_num = Adrian_exam_current_num + 1
-  elif i % 3 == 1:
-    if 'B' == exam_consists[i]:
-      Adrian_exam_current_num = Adrian_exam_current_num + 1
-  elif i % 3 == 2:
-    if 'C' == exam_consists[i]:
-      Adrian_exam_current_num = Adrian_exam_current_num + 1      
-
-# Bruno
 Bruno_exam_current_num = 0
-for i in range(exam_num):
-  if i % 4 == 0:
-    if 'B' == exam_consists[i]:
-      Bruno_exam_current_num = Bruno_exam_current_num + 1
-  elif i % 4 == 1:
-    if 'A' == exam_consists[i]:
-      Bruno_exam_current_num = Bruno_exam_current_num + 1
-  elif i % 4 == 2:
-    if 'B' == exam_consists[i]:
-      Bruno_exam_current_num = Bruno_exam_current_num + 1 
-  elif i % 4 == 3:
-    if 'C' == exam_consists[i]:
-      Bruno_exam_current_num = Bruno_exam_current_num + 1 
-
-# Goran
 Goran_exam_current_num = 0
+
 for i in range(exam_num):
-  if i % 6 == 0:
-    if 'C' == exam_consists[i]:
-      Goran_exam_current_num = Goran_exam_current_num + 1
-  elif i % 6 == 1:
-    if 'C' == exam_consists[i]:
-      Goran_exam_current_num = Goran_exam_current_num + 1
-  elif i % 6 == 2:
-    if 'A' == exam_consists[i]:
-      Goran_exam_current_num = Goran_exam_current_num + 1 
-  elif i % 6 == 3:
-    if 'A' == exam_consists[i]:
-      Goran_exam_current_num = Goran_exam_current_num + 1 
-  elif i % 6 == 4:
-    if 'B' == exam_consists[i]:
-      Goran_exam_current_num = Goran_exam_current_num + 1 
-  elif i % 6 == 5:
-    if 'B' == exam_consists[i]:
-      Goran_exam_current_num = Goran_exam_current_num + 1
+# Adrian
+  if exam_consists[i] == ADRIAN[i % len(ADRIAN)]:
+    Adrian_exam_current_num = Adrian_exam_current_num + 1
+# Bruno
+  if exam_consists[i] == BRUNO[i % len(BRUNO)]:
+    Bruno_exam_current_num = Bruno_exam_current_num + 1
+# Goran
+  if exam_consists[i] == GORAN[i % len(GORAN)]:
+    Goran_exam_current_num = Goran_exam_current_num + 1
 
 exam_current_max = max(Adrian_exam_current_num, max(Bruno_exam_current_num, Goran_exam_current_num))
 
